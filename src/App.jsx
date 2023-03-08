f6496d916536b6c451961a31be272b078006a36d
Gw3i <contact.johngoldco@gmail.com>
2023-03-08 22:37:26 +0100

    Add TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import TodoForm from "./Components/TodoForm";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <TodoForm />
+    </>
+  );
 }
 
 export default App;
