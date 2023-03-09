9f412bd0eb28919f1d2605f7f9b3d01b788fbaf5
Swappnet <advance11ua@gmail.com>
2023-03-09 17:32:35 +0200

    added spinner loader

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,19 @@ function App() {
   const [user, loading, error] = useAuthState(auth);
 
   if (loading) {
-    // Change in future to styled loader
-    return <div>Loading...</div>;
+    // Change in future to nice styled loader
+    return (
+      <div class="flex items-center justify-center mt-10">
+        <div
+          class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
   }
 
   return (
