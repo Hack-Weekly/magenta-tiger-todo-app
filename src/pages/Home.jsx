7297b9343372176b6fe0ef88416675176bc22dd7
Swappnet <advance11ua@gmail.com>
2023-03-10 21:02:13 +0200

    Optimized function for task fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,52 @@
-import TodoForm from "../Components/TodoForm"
-import TodoList from "../Components/TodoList"
+import TodoForm from '../Components/TodoForm';
+import TodoList from '../Components/TodoList';
 
-import { useNavigate } from "react-router-dom"
-import { useEffect, useState } from "react"
-import { auth, logout } from "../firebase/auth"
-import { useAuthState } from "react-firebase-hooks/auth"
-import { getTasks } from "../firebase/firestore"
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { auth, logout } from '../firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getTasks } from '../firebase/firestore';
 
 const Home = () => {
-    const [user, loading, error] = useAuthState(auth)
-    const [userID, setUserID] = useState(null)
-    const [tasks, setTasks] = useState(null)
+  const [user, loading, error] = useAuthState(auth);
+  const [userID, setUserID] = useState(null);
+  const [tasks, setTasks] = useState(null);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const getTasksFromFirebase = async id => {
-        const tasks = await getTasks(id)
-        if (tasks) {
-            setTasks(tasks)
-        } else if (!tasks) {
-            return null
-        }
+  const getTasksFromFirebase = async () => {
+    if (user) {
+      const fetchedTasks = await getTasks(user.uid);
+      if (fetchedTasks) {
+        setTasks(fetchedTasks);
+      } else {
+        return null;
+      }
     }
+  };
 
-    useEffect(() => {
-        if (!user) {
-            navigate("/login")
-        }
-        if (user) {
-            setUserID(user.uid)
-            getTasksFromFirebase(user.uid)
-        }
-    }, [user])
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+    if (user) {
+      setUserID(user.uid);
+      getTasksFromFirebase(user.uid);
+    }
+  }, [user]);
 
-    return (
-        <>
-            <TodoForm
-                userUID={userID}
-                getTasksFromFirebase={getTasksFromFirebase}
-            />
-            <TodoList tasks={tasks} />
-            <button
-                onClick={logout}
-                className="rounded-md bg-red-500 text-white px-4"
-            >
-                Log Out
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <TodoForm userUID={userID} getTasksFromFirebase={getTasksFromFirebase} />
+      <TodoList tasks={tasks} getTasksFromFirebase={getTasksFromFirebase} />
+      <button
+        onClick={logout}
+        className="rounded-md bg-red-500 text-white px-4"
+      >
+        Log Out
+      </button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
