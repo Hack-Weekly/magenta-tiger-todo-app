f4ec80f2df914f61f261e04956b1b869eace6dc9
Swappnet <advance11ua@gmail.com>
2023-03-11 19:09:34 +0200

    Added func to convert date into 'Due date'

--- src/Components/utilFunctions/dateConvert.js
+++ src/Components/utilFunctions/dateConvert.js
@@ -1,7 +1,27 @@
 export default function dateConvert(timestamp) {
-    const date = new Date(timestamp)
-    const options = { day: "numeric", month: "long", year: "numeric" }
-    const formattedDate = date.toLocaleDateString("en-GB", options)
+  const date = new Date(timestamp);
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-GB', options);
 
-    return formattedDate
+  return formattedDate;
+}
+
+export function formatDueDate(dateObj) {
+  const now = new Date();
+  const convertedDate = new Date(dateObj.seconds * 1000);
+
+  if (now.toDateString() === convertedDate.toDateString()) {
+    return 'Due Today';
+  } else {
+    const convertedDateString = convertedDate.toLocaleDateString('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    });
+    if (now < convertedDate) {
+      return `Due ${convertedDateString}`;
+    } else {
+      return `Overdue ${convertedDateString}`;
+    }
+  }
 }
