082111e82987c24ad03af07bab36c82faa41597a
Gw3i <contact.johngoldco@gmail.com>
2023-03-13 00:48:17 +0100

    Fix scroll behaviour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,14 +40,14 @@ const Home = () => {
 
   return (
     // change styling as appropriate//
-    <div className="relative h-screen overflow-hidden md:grid md:grid-cols-[auto,1fr] md:gap-5">
+    <div className="relative h-screen overflow-x-hidden  md:grid md:grid-cols-[auto,1fr] md:gap-5">
       <Nav tasks={tasks} onSelectedTag={setSelectedTag} />
       <main>
         <TodoForm
           userUID={userID}
           getTasksFromFirebase={getTasksFromFirebase}
         />
-        <div className="px-5 mt-8">
+        <div className="px-5 mt-8 mb-5">
           <TodoList
             tasks={tasks}
             getTasksFromFirebase={getTasksFromFirebase}
