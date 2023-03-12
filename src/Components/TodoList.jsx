c9e7f45985fa2ddb5280d215f63f9ace5862f3c0
Gw3i <contact.johngoldco@gmail.com>
2023-03-13 00:14:01 +0100

    Filter todoList

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -39,86 +39,119 @@ const TodoList = ({ tasks, getTasksFromFirebase, selectedTag }) => {
             {selectedTag}
           </h2>
         )}
-        <h2 className="text-xl font-bold text-light-grey">Open</h2>
-        <ul className="list-disc">
-          {todos
-            ? todos
-                .filter((todo) => !todo.isCompleted)
-                .sort((a, b) => b.date - a.date)
-                .map((todo) => {
-                  console.log(todo.tag);
+        {selectedTag !== 'completed' && (
+          <>
+            <h2 className="text-xl font-bold text-light-grey">Open</h2>
 
-                  return (
-                    <li
-                      className="list-inside flex justify-between mb-5 items-center"
-                      key={todo.id}
-                    >
-                      <div className="flex gap-2">
-                        <input
-                          id={todo.name}
-                          type="checkbox"
-                          onChange={() => {
-                            markAsCompleted(todo);
-                          }}
-                        />
-                        <div className="flex flex-col">
-                          <label
-                            htmlFor={todo.name}
-                            className="text-sm sm:text-base"
-                          >
-                            {todo.name}
-                          </label>
-                          <div className="flex text-xs gap-[2px] ">
-                            <p className="first-letter:uppercase whitespace-nowrap">
-                              {todo.tag} <span>•</span>
-                            </p>
-                            <p className="text-xs text-blue ">
-                              {dateConvert(todo.date)}
-                            </p>
-                          </div>
-                          {todo.dueDate.length !== 0 && (
-                            <p className="text-xs">
-                              {formatDueDate(todo.dueDate)}
-                            </p>
-                          )}
-                        </div>
-                      </div>
+            <ul className="list-disc">
+              {todos
+                ? todos
+                    .filter((todo) => {
+                      if (!selectedTag || selectedTag === 'all') {
+                        return !todo.isCompleted;
+                      }
 
-                      <div className="flex gap-2">
-                        <div className="grid justify-items-end gap-[6px]">
-                          <TodoEdit
-                            dueDate={todo.dueDate}
-                            docID={todo.docID}
-                            name={todo.name}
-                            getTasksFromFirebase={getTasksFromFirebase}
-                            tag={todo.tag}
-                          />
-                          <Button
-                            type="danger"
-                            btnText="Delete"
-                            onClick={() => handleDelete(todo.docID)}
-                          />
-                        </div>
+                      if (selectedTag === 'favorite') {
+                        return !todo.isCompleted && todo.isFavourited;
+                      }
 
-                        <button
-                          onClick={() => {
-                            markAsFavourited(todo);
-                          }}
+                      if (!todo.isCompleted && selectedTag === todo.tag) {
+                        return !todo.isCompleted && selectedTag === todo.tag;
+                      }
+                    })
+                    .sort((a, b) => b.date - a.date)
+                    .map((todo) => {
+                      console.log(todo.tag);
+
+                      return (
+                        <li
+                          className="list-inside flex justify-between mb-5 items-center"
+                          key={todo.id}
                         >
-                          {todo.isFavourited ? fullStar : hollowStar}
-                        </button>
-                      </div>
-                    </li>
-                  );
-                })
-            : null}
-        </ul>
+                          <div className="flex gap-2">
+                            <input
+                              id={todo.name}
+                              type="checkbox"
+                              onChange={() => {
+                                markAsCompleted(todo);
+                              }}
+                            />
+                            <div className="flex flex-col">
+                              <label
+                                htmlFor={todo.name}
+                                className="text-sm sm:text-base"
+                              >
+                                {todo.name}
+                              </label>
+                              <div className="flex text-xs gap-[2px] ">
+                                <p className="first-letter:uppercase whitespace-nowrap">
+                                  {todo.tag} <span>•</span>
+                                </p>
+                                <p className="text-xs text-blue ">
+                                  {dateConvert(todo.date)}
+                                </p>
+                              </div>
+                              {todo.dueDate.length !== 0 && (
+                                <p className="text-xs">
+                                  {formatDueDate(todo.dueDate)}
+                                </p>
+                              )}
+                            </div>
+                          </div>
+
+                          <div className="flex gap-2">
+                            <div className="grid justify-items-end gap-[6px]">
+                              <TodoEdit
+                                dueDate={todo.dueDate}
+                                docID={todo.docID}
+                                name={todo.name}
+                                getTasksFromFirebase={getTasksFromFirebase}
+                                tag={todo.tag}
+                              />
+                              <Button
+                                type="danger"
+                                btnText="Delete"
+                                onClick={() => handleDelete(todo.docID)}
+                              />
+                            </div>
+
+                            <button
+                              onClick={() => {
+                                markAsFavourited(todo);
+                              }}
+                            >
+                              {todo.isFavourited ? fullStar : hollowStar}
+                            </button>
+                          </div>
+                        </li>
+                      );
+                    })
+                : null}
+            </ul>
+          </>
+        )}
 
         <h2 className="text-xl font-bold text-light-grey">Done</h2>
         <ul className="list-disc">
           {todos
             ? todos
-                .filter((todo) => todo.isCompleted)
+                .filter((todo) => {
+                  if (
+                    !selectedTag ||
+                    selectedTag === 'all' ||
+                    selectedTag === 'completed'
+                  ) {
+                    return todo.isCompleted;
+                  }
+
+                  if (selectedTag === 'favorite') {
+                    return todo.isCompleted && todo.isFavourited;
+                  }
+
+                  if (todo.isCompleted && selectedTag === todo.tag) {
+                    return todo.isCompleted && selectedTag === todo.tag;
+                  }
+                })
                 .sort((a, b) => b.date - a.date)
                 .map((todo) => {
                   return (
